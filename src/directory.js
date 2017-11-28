f0d858fb417a1cac39d4010a5723255914c0ea22
vasily <bacrilio@gmail.com>
2017-11-28 19:16:14 +0300

    update

--- src/directory.js
+++ src/directory.js
@@ -1,52 +1,68 @@
 /**
- * @api {get} /doctors/ Загрузка справочника докторов
+ * @api {post} / Загрузка справочника докторов
  * @apiVersion 0.0.1
  * @apiName GetDoctors
  * @apiGroup Directory
  * @apiPermission none
+ * @apiDescription Без передачи списка айдишников докторов возвращяет базовые данные по всем докторам, при передаче списка возвращает полные данные для переданных айдишников
+ * 
+ * @apiParam {String='fA$@DQ|yFl3Y4ngNuEKaf#7j'}        token
+ * @apiParam {String}       url
+ * @apiParam {String[]}     [ids]                          Массив айдишников докторов
  *
- * @apiSuccess {Array}      list                        Массив с данными по докторам      
- * @apiSuccess {Number}     list.id                     Айди
- * @apiSuccess {String}     list.doctor_fio             Полное имя врача.
- * @apiSuccess {String}     list.doctor_fio_short       Сокращенное имя врача
- * @apiSuccess {Array}      list.specialization_ids     Айди специализаций - детали в другом справочнике       
- * @apiSuccess {String}     list.avatar                 Ссылка на аватарку
- * @apiSuccess {String}     list.position               Должность
- * @apiSuccess {String}     list.degree                 Степень
- * @apiSuccess {Number}     list.experience             Стаж
- * @apiSuccess {Number}     list.rate                   Рейтинг
- * @apiSuccess {Array}      list.achievements           Айди достижений - детали в другом справочнике
- * @apiSuccess {Number}     list.binding                Айди поликлиники (основная привязка)
- * @apiSuccess {Number}     list.price                  Стоимость приема
- * @apiSuccess {String}     list.description            Общая информация о враче               
- * @apiSuccess {String}     list.employment             Информация чем он занимается в клинике (HTML)                        
+ * @apiSuccess {Object[]}   list                            Массив с данными по докторам      
+ * @apiSuccess {Number}     list.id                         Айди
+ * @apiSuccess {String}     list.doctor_fio                 Полное имя врача.
+ * @apiSuccess {String}     list.doctor_fio_short           Сокращенное имя врача
+ * @apiSuccess {Number[]}   list.specializations            Айди специализаций - детали в другом справочнике       
+ * @apiSuccess {String}     list.avatar                     Ссылка на аватарку
+ * @apiSuccess {Number}     list.rate                       Рейтинг
+ * @apiSuccess {Number[]}   list.achievements               Айди достижений - детали в другом справочнике
+ * @apiSuccess {String}     [list.position]                 Должность
+ * @apiSuccess {String}     [list.degree]                   Степень
+ * @apiSuccess {Number}     [list.experience]               Стаж
+ * @apiSuccess {Number[]}   [list.clinics]                  Массив айдишников клиник в которых работает этот врач
+ * @apiSuccess {Number}     [list.price]                    Стоимость приема
+ * @apiSuccess {String}     [list.description]              Общая информация о враче               
+ * @apiSuccess {InfoBlock[]}     [list.informations]        Массив информационных блоков    
+ *                             
+ * @apiSuccess (InfoBlock) {String}     [title]             Заголовок информационного блока (опция)                                  
+ * @apiSuccess (InfoBlock) {String}     [description]       Описание информационного блока (опция)                                  
+ * @apiSuccess (InfoBlock) {String[]}   [list]              Список данных по инфоблоку (строки) (опция)                                                           
  *
  */
 
  /**
- * @api {get} /user/schedule/specializations Загрузка справочника специализаций
+ * @api {post} / Загрузка справочника специализаций
  * @apiVersion 0.0.1
  * @apiName GetSpecializations
  * @apiGroup Directory
  * @apiPermission none
  * @apiDescription метод существует
  *
- * @apiSuccess {Array}      list        Массив с данными по специализациям      
+ * @apiParam {String='fA$@DQ|yFl3Y4ngNuEKaf#7j'}        token
+ * @apiParam {String='user/schedule/specializations'}   url
+ * 
+ * @apiSuccess {Object[]}   list        Массив с данными по специализациям      
  * @apiSuccess {Number}     list.id     Айди
  * @apiSuccess {String}     list.name   Название специализации                   
  *
  */
 
  /**
- * @api {get} /achievements/ Загрузка справочника достижений
+ * @api {get} / Загрузка справочника достижений
  * @apiVersion 0.0.1
  * @apiName GetAchievements
  * @apiGroup Directory
  * @apiPermission none
+ * 
+ * @apiParam {String='fA$@DQ|yFl3Y4ngNuEKaf#7j'}        token
+ * @apiParam {String}   url
  *
- * @apiSuccess {Array}      list            Массив с данными по достижениям      
- * @apiSuccess {Number}     list.id         Айди
- * @apiSuccess {String}     list.color      Цвет метки                  
- * @apiSuccess {String}     list.name       Название достижения                   
+ * @apiSuccess {Object[]}   list                Массив с данными по достижениям      
+ * @apiSuccess {Number}     list.id             Айди
+ * @apiSuccess {String}     list.color          Цвет метки                  
+ * @apiSuccess {String}     list.name           Название достижения                   
+ * @apiSuccess {String}     list.descriprion    Описание достижения                   
  *
  */
